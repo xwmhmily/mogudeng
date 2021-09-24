1dd8199e80781a672c23d7c1302d2f358938bd28
xwmhmily@126.com <381345509@qq.com>
2021-09-24 13:49:32 +0800

    Add nlp cut words demo

--- nlp.php
+++ nlp.php
@@ -1,5 +1,5 @@
 <?php
 
-$text = $_GET['test'];
+$text = $_GET['text'];
 
-echo file_get_contents("http://116.62.138.92:9502/nlp/parse?text=".$text); 
+echo file_get_contents("http://116.62.138.92:9502/nlp/parse?text=".$text);
